23508592ce29c32ee03c05e0177b42cf257498d6
avivdro <drori.aviv@gmail.com>
2021-05-20 15:43:48 +0300

    first commit

---
+++ whatsapp2.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\Aviv\\Downloads\\chromedriver_win32\\chromedriver.exe')
+driver.implicitly_wait(15)
+driver.get('https://web.whatsapp.com')
+
+while True:
+    try:
+        driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
+        inputString = input("Enter message to send: ")
+        for i in range(0, int(input("how many msgs to send?"))):
+            driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
+            driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+    except Exception:
+        print("cant do the thing")
+    finally:
+        print("lets do it again!:")
